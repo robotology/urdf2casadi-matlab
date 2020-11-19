a17d9df4c1f10464e9bd5640e0921a2dd4afecb4
Baljinder <singhbal.baljinder@gmail.com>
2020-11-19 15:46:30 +0100

    Add icub right leg urdf and improve plot

--- Verfication/functionForTests/testMassAndCoriolisMatricesOnMatlab.m
+++ Verfication/functionForTests/testMassAndCoriolisMatricesOnMatlab.m
@@ -3,7 +3,8 @@
 kuka_urdf = '/home/iiticublap041/idjl-model-identification/results/identification_results/kuka_kr30_ha/urdf/kr30_ha-identified.urdf';
 twoLink_urdf = '/home/iiticublap041/baljinder/urdf2casadi-matlab/URDFs/twoLinks.urdf';
 kuka_kr210 = '/home/iiticublap041/baljinder/urdf2casadi-matlab/URDFs/kuka_kr210.urdf';
-robotURDFModel=kuka_urdf;
+iCub_r_leg = '/home/iiticublap041/baljinder/urdf2casadi-matlab/URDFs/iCub_r_leg.urdf';
+robotURDFModel=iCub_r_leg;
 
 gravityModulus = 9.80665;
 tol = 1e-10;
@@ -17,7 +18,7 @@ g = [0;0;-gravityModulus];
 
 
 %% Prepare variables to store results
-nrOfTests = 10;
+nrOfTests = 20;
 e_massMatrix = zeros(nrOfJoints,nrOfJoints,nrOfTests);
 e_gravity    = zeros(nrOfJoints,nrOfTests);
 e_torqueID   = zeros(nrOfJoints,nrOfTests);
@@ -59,3 +60,4 @@ for i= 1:nrOfTests
     e_torqueID(:,i) = abs(tau_efficient-tau_ID);
     e_genBias(:,i) = abs(generalizedBias-genealizedBias_IDyn);
 end
+plot(e_normMass');title('Mass matrix error norm: norm(M_{symb} - M_{IDyn})');legend;   
