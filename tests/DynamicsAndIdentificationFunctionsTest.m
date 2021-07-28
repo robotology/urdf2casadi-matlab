659675f68b2950d9dca701afab797f287a214d7a
singhbal.baljinder <singhbal.baljinder>
2021-07-28 18:25:15 +0200

    Add more tests
    CI workflow

--- tests/DynamicsAndIdentificationFunctionsTest.m
+++ tests/DynamicsAndIdentificationFunctionsTest.m
@@ -1,4 +1,4 @@
-classdef DynamicsFunctionsTest < matlab.unittest.TestCase
+classdef DynamicsAndIdentificationFunctionsTest < matlab.unittest.TestCase
     %DynamicsFunctionTest Test Robot Dynamics Model functions
     %   The functions used in computing the various components and quontities of the
     %   robot dynamics model againts the same or similar functionaties
@@ -24,7 +24,7 @@ classdef DynamicsFunctionsTest < matlab.unittest.TestCase
         function setUpVariablesForTests(testCase)
             % Fix location of the folder to store the generated c and .mex files
             testCase.location_tests_folder = pwd;
-            testCase.location_generated_functions = [testCase.location_tests_folder,'/../../automaticallyGeneratedFunctions'];
+            testCase.location_generated_functions = [testCase.location_tests_folder,'/../automaticallyGeneratedFunctions'];
 
             % Fix location of urdf to be used in the tests
             twoLink_urdf = [testCase.location_tests_folder,'/../URDFs/twoLinks.urdf'];
@@ -144,13 +144,8 @@ classdef DynamicsFunctionsTest < matlab.unittest.TestCase
                 % Gravity column vector
                 g =[0; 0; -testCase.gravityModulus];
                 
-                jointPos = rand(nrOfJoints,1);
-                jointVel = rand(nrOfJoints,1);
-                jointAcc = rand(nrOfJoints,1);
-                
                 % Test with symbolic function
                 % If we do not supply the external forces they are NOT automatically considered null in the symbolic function
-                nrOfJoints = size(jointPos,1);
                 extForce = zeros(6,nrOfJoints);
                 
                 for ii = 1:nrOfJoints
@@ -165,6 +160,11 @@ classdef DynamicsFunctionsTest < matlab.unittest.TestCase
                 tau_RNEA_list = zeros(nrOfJoints, testCase.nrOfTests);
 
                 for ii = 1 : testCase.nrOfTests
+                    
+                    jointPos = rand(nrOfJoints,1);
+                    jointVel = rand(nrOfJoints,1);
+                    jointAcc = rand(nrOfJoints,1);
+                
                     tau_RNEA = symbolicIDFunction(jointPos, jointVel, jointAcc, g, extForce);
                     tau_RNEA = full(tau_RNEA);
 
@@ -180,6 +180,42 @@ classdef DynamicsFunctionsTest < matlab.unittest.TestCase
             
         end
         
+        function testInverseDynamicsInertiaParametersRegressor_2(testCase)
+            % A second test on the Inverse Dynamics regressor
+            
+            % Import necessary functions
+            import urdf2casadi.Utils.inverseDynamicsInertialParametersRegressor
+            import urdf2casadi.Utils.iDynTreeDynamicsFunctions.computeRegressorIdynTree
+                
+            for r = 1 : length(testCase.testURDFfiles)
+                
+                robotURDFModel = testCase.testURDFfiles{r};
+
+                % Load iDynTree model
+                testCase.mdlLoader.loadModelFromFile(robotURDFModel);
+                testCase.kinDynComp.loadRobotModel(testCase.mdlLoader.model());
+
+                nrOfJoints = testCase.kinDynComp.model().getNrOfDOFs();
+            
+                Y = inverseDynamicsInertialParametersRegressor(robotURDFModel, true, testCase.location_generated_functions);
+                
+                g = [0; 0; -testCase.gravityModulus];
+                
+                for t = 1: testCase.nrOfTests
+                    jointPos = rand(nrOfJoints,1);
+                    jointVel = rand(nrOfJoints,1);
+                    jointAcc = rand(nrOfJoints,1);
+                    
+                    Y_symb = Y(jointPos, jointVel, jointAcc, g);
+
+                    Y_IDyn = computeRegressorIdynTree(robotURDFModel, jointPos, jointVel, jointAcc, testCase.gravityModulus);
+
+                    regressor_matrix_error = abs(Y_IDyn(:,11:end)-full(Y_symb));
+                    
+                    assertLessThanOrEqual(testCase, regressor_matrix_error,  testCase.tol);
+                end
+            end
+        end
         function testMassAndCoriolisMatrices(testCase)
             % Test the Mass and the Coriolis matrices
 
@@ -257,6 +293,120 @@ classdef DynamicsFunctionsTest < matlab.unittest.TestCase
                 assertLessThanOrEqual(testCase, e_torqueID,  testCase.tol);
                 assertLessThanOrEqual(testCase, e_genBias,  testCase.tol);
             
+            end
+        end
+        
+        function testSingleLinkRegressor(testCase)
+            %% Import necesary functions
+            import urdf2casadi.Utils.modelExtractionFunctions.extractSystemModel
+            import urdf2casadi.Utils.computeKinematics
+            import urdf2casadi.Identification.auxiliarySymbolicDynamicsFunctions.computeLinkRegressor
+            import urdf2casadi.Utils.Spatial.crf
+
+            for r = 1 : length(testCase.testURDFfiles)
+                
+                robotURDFModel = testCase.testURDFfiles{r};
+                
+                %Load urdf and convert to SMDS format
+                smds = extractSystemModel(robotURDFModel);
+                nrOfJoints = smds.NB;
+
+                g = [0; 0; -testCase.gravityModulus];
+
+                for t = 1 : testCase.nrOfTests
+                    jointPos = rand(nrOfJoints, 1);
+                    jointVel = rand(nrOfJoints, 1);
+                    jointAcc = rand(nrOfJoints, 1);
+
+                    % Compute frame transformations, joint motion subspace matrix, link velocity and accelerations
+                    [X,~,~,Xup, v, a] = computeKinematics(smds, jointPos, jointVel, jointAcc, g);
+
+                    % Compute for each link i the matrix A_j for all links j distal to link
+                    % i. This matix arises from recasting the spatial Newton-Euler dynamic equation 
+                    % of each link i in a form linear wrt the Inertial paramter vector of link i, expressed in the local frame i 
+                    for l = 1:smds.NB
+                        % Gravity in local frame
+                        g_l = (X{1}{1,l}*Xup{1})*[0;0;0;g];
+                        % Velocity and acceleration must be expressed in the same frame as the
+                        % inertia and the center of mass: the body i local frame
+                        A{l} = computeLinkRegressor( v{l}, a{l});
+                        % Test the regressor 
+                        p = [smds.mass{l}; smds.mass{l}*smds.com{l}; smds.I{l}(1,1); smds.I{l}(1,2); smds.I{l}(1,3);...
+                                               smds.I{l}(2,2); smds.I{l}(2,3); smds.I{l}(3,3)];
+                        f_regressor = A{l}*p;
+                        f_NE = smds.I{l}*a{l} + crf(v{l})*smds.I{l}*v{l};   
+                    end
+
+                    % Check if the Newton-Euler and regressor based dynamics equations give the
+                    % same result
+                    assertLessThanOrEqual(testCase, f_regressor - f_NE,  testCase.tol);
+                end
+            end
+        end
+        
+        function testJacobian(testCase)
+
+            for r = 1 : length(testCase.testURDFfiles)
+                
+                robotURDFModel = testCase.testURDFfiles{r};
+                
+                opts.geneate_c_code = true;
+                opts.location_generated_fucntion = testCase.location_generated_functions;
+                opts.FrameVelocityRepresentation = "MIXED_REPRESENTATION";
+                [J_symb,X,XForce,S,O_X_ee] = urdf2casadi.Dynamics.auxiliarySymbolicDynamicsFunctions.createSpatialTransformsFunction(robotURDFModel, opts);
+
+                % IDynTree variables
+                testCase.mdlLoader.loadModelFromFile(robotURDFModel);
+                testCase.kinDynComp.loadRobotModel(testCase.mdlLoader.model());
+
+                % Set the body-fixed frame (left-trivialized velocity) representation
+                testCase.kinDynComp.setFrameVelocityRepresentation(iDynTree.MIXED_REPRESENTATION);
+                model_iDyn = testCase.kinDynComp.model();
+
+                nrOfJoints = model_iDyn.getNrOfJoints();
+                q_idyn = iDynTree.VectorDynSize(nrOfJoints);
+                dq_idyn = iDynTree.VectorDynSize(nrOfJoints);
+                grav = iDynTree.Vector3();
+                J_idyn = iDynTree.MatrixDynSize();
+
+
+                % Constants
+                % Assume that the base frame has index 0 and the
+                % end-effector frame has index nrOfJoints
+                baseFrame = 0;
+                eeFrame = nrOfJoints;
+                grav.fromMatlab([0; 0; -testCase.gravityModulus]);
+                
+                J_iDyn_list = zeros(6,nrOfJoints, testCase.nrOfTests);
+                J_symb_list = zeros(6,nrOfJoints, testCase.nrOfTests);
+                e_jac = zeros(1, testCase.nrOfTests);
+                
+                for t = 1 : testCase.nrOfTests
+
+                    jointPos = rand(nrOfJoints,1);
+
+                    % Compute jacobian with iDynTree
+                    q_idyn.fromMatlab(jointPos);
+                    dq_idyn.fromMatlab(zeros(nrOfJoints,1));
+                    
+
+                    testCase.kinDynComp.setRobotState(q_idyn, dq_idyn, grav);
+                    testCase.kinDynComp.getRelativeJacobian(baseFrame, eeFrame, J_idyn);
+                    J_tmp = J_idyn.toMatlab();
+
+                    % Featherstone's notation considers first the angular part
+                    J_iDyn_list(4:6,:,t) = J_tmp(1:3,:);
+                    J_iDyn_list(1:3,:,t) = J_tmp(4:6,:);
+
+                    % Compute Jacobian with symbolic function
+                    J_symb_list(:,:,t) = full(J_symb(jointPos));
+
+                    % Error in the norm of the difference of the Jacobians
+                    e_jac(:,t)  = norm(J_symb_list(:,:,t) - J_iDyn_list(:,:,t));
+
+                end
+                
+                assertLessThanOrEqual(testCase, e_jac,  testCase.tol);
             end
         end
     end
